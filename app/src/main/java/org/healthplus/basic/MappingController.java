91639a7343c595f84a523707bd4dff77809f168d
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-10-06 14:36:17 +0900

    feat : mapping Info

--- app/src/main/java/org/healthplus/basic/MappingController.java
+++ app/src/main/java/org/healthplus/basic/MappingController.java
@@ -2,6 +2,8 @@ package org.healthplus.basic;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,24 @@ public class MappingController {
   @GetMapping("/mapping-get-v2")
   public String mappingGetV2() {
     log.info("mappingGetV2");
+    return "ok";
+  }
+
+  @GetMapping("/mapping/{userId}")
+  public String mappingPath(@PathVariable("userId") String data) {
+    log.info("PathVariable Data = {}", data);
+    return "OK";
+  }
+
+  @GetMapping("/mapping/{userId}/orders/{age}")
+  public String mappingPath2(@PathVariable String userId, @PathVariable Integer age) {
+    log.info("PathVar UserId = {}, Age = {}", userId, age);
+    return "OK!";
+  }
+
+  @PostMapping(value = "/mapping-consume", consumes = "application/json")
+  public String mappingConsumes() {
+    log.info("mappingConsumes");
     return "ok";
   }
 }
